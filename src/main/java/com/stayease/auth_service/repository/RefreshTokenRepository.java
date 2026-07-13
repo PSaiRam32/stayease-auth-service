4b9bc4f5015475a4a0cdd794fea582bf234d058f
PSaiRam32 <2000030757@kluniversity.in>
2026-07-13 15:35:57 +0530

    Logout and Refresh Token Persistence Feature Implementation

--- src/main/java/com/stayease/auth_service/repository/RefreshTokenRepository.java
+++ src/main/java/com/stayease/auth_service/repository/RefreshTokenRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long>{
     Optional<RefreshToken> findByToken(String token);
-    List<RefreshToken> findByUserUserId(Long userId);
     void deleteByUserUserId(Long userId);
 }
